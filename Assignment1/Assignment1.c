ac80db92a4568291bfa21525c5019427c67c2927
Sean Roach <sproach@iu.edu>
2019-01-15 14:54:14 -0500

    This is Assignment1

---
+++ Assignment1/Assignment1.c
@@ -0,0 +1,22 @@
+//first thing
+#include<stdio.h>
+int main()
+{
+printf("{Hello/\Welcome back to IU!}, This is Spring 2019's 1st 8 Week Course\n"); 
+int a = 10;
+while( a = 10 ) {
+    printf("Sean");
+    fflush(stdout);
+    sleep(2);
+    int i;
+    for(i=0; i<5; i++) {
+        printf(".");
+        fflush(stdout);
+        sleep(2);
+    }
+    printf("\n");
+}
+return 0;
+}
+//gcc to compile,
+//gcc hello -o hello
